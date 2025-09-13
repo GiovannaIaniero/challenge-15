58ae8912fd3b51e7850c1806adc3ae7666cfd192
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-09-13 13:13:44 -0300

    resuelvo primer ítems de ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,18 @@
+/* GESTIÓN DE TAREAS 
+Mostrar las tareas que están pendientes (no completadas)
+Crear un array solo con las descripciones de las tareas
+Filtrar las tareas de alta prioridad que no están completadas
+Encontrar la tarea que contiene "JavaScript" en su descripción*/
+
+const tareas = [{ id: 1, descripcion: "Hacer ejercicio", completada: true, prioridad: "alta" },
+{ id: 2, descripcion: "Estudiar JavaScript", completada: false, prioridad: "alta" },
+{ id: 3, descripcion: "Comprar víveres", completada: false, prioridad: "media" },
+{ id: 4, descripcion: "Llamar al médico", completada: true, prioridad: "baja" },
+{ id: 5, descripcion: "Limpiar la casa", completada: false, prioridad: "media" }]
+
+/*Mostrar las tareas que están pendientes (no completadas)*/
+
+const tareasPendientes = tareas.filter(tareas => !tareas.completada);
+console.log(tareasPendientes);
+
+
